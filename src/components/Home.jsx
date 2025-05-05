77d48e4181114f127ed6fbc273b75fc94a73e0f9
archita-b <archita.bhattacharyya91@gmail.com>
2025-05-05 16:35:17 +0530

    style home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      <nav>
+    <div className="container">
+      <nav className="navbar">
         <Link to="/stopwatch">Stopwatch</Link>
         <Link to="/timer">Timer</Link>
       </nav>
