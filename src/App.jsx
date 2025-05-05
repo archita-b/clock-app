757727fd12103b2313c9cd4c58e1e359084fae8b
archita-b <archita.bhattacharyya91@gmail.com>
2025-05-05 16:47:01 +0530

    create pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import StopWatch from "./components/StopWatch";
-import Timer from "./components/Timer";
+import Home from "./pages/home/Home";
+import StopWatch from "./pages/stopwatch/StopWatch";
+import Timer from "./pages/timer/Timer";
 
 function App() {
   return (
